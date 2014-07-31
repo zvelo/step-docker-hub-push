15421e6d5250b233ef37dd2acda570890ee2252a
Carl Saturnino <csaturnino@zvelo.com>
2014-07-31 12:21:01 +0800

    wercker step definition and run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+set -e
+cd $HOME
+
+if [ ! -n "$DOCKER_EMAIL" ]
+then
+    error 'Please specify docker email'
+    exit 1
+fi
+
+if [ ! -n "$DOCKER_USERNAME" ]
+then
+    error 'Please specify docker username'
+    exit 1
+fi
+
+if [ ! -n "$DOCKER_PASSWORD" ]
+then
+    error 'Please specify docker password'
+    exit 1
+fi
+
+docker login --email="$DOCKER_EMAIL" --password="$DOCKER_PASSWORD" --username="$DOCKER_USERNAME"
+
+set -e
