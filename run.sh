393ff84c5803ceb4fbf6494b8578f5e2a53f8d17
Carl Saturnino <csaturnino@zvelo.com>
2014-08-01 12:35:21 +0800

    Remove double quotes around environment variables

--- run.sh
+++ run.sh
@@ -27,17 +27,19 @@ fi
 
 docker_push() {
     set +e;
-    local user="$1";
-    local pass="$2";
-    local email="$3";
-    local image="$4";
+    local user=$1;
+    local pass=$2;
+    local email=$3;
+    local image=$4;
 
     docker version
     docker images
-    docker login --username="$user" --password="$pass" --email="$email";
+    echo "Email: $email"
+    echo 'docker login --username=$user'
+    docker login --username=$user --password=$pass --email=$email;
     docker push $image;
     set -e;
 }
 
-docker_push "$WERCKER_DOCKER_HUB_PUSH_USERNAME" "$WERCKER_DOCKER_HUB_PUSH_PASSWORD" "$WERCKER_DOCKER_HUB_PUSH_EMAIL" "$WERCKER_DOCKER_HUB_PUSH_IMAGE";
+docker_push $WERCKER_DOCKER_HUB_PUSH_USERNAME $WERCKER_DOCKER_HUB_PUSH_PASSWORD $WERCKER_DOCKER_HUB_PUSH_EMAIL $WERCKER_DOCKER_HUB_PUSH_IMAGE;
 
